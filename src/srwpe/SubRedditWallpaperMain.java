09b803b1fed5d8b97f03b7df65866ee9e769b150
creye111 <car168490@gmail.com>
2021-06-03 22:29:08 -0400

    added scrape functionality to the run button

--- src/srwpe/SubRedditWallpaperMain.java
+++ src/srwpe/SubRedditWallpaperMain.java
@@ -1,6 +1,8 @@
 package srwpe;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -14,6 +16,7 @@ public class SubRedditWallpaperMain extends Application{
 	private String appName="Subreddit Wallpaper Grabber";
 	private String defaultSubLink = "https://www.reddit.com/r/EarthPorn/";
 	private String titleCSSStyle ="-fx-font: 18 \"Comic Sans MS\";";
+	TextField subredditLinkInput;
 	@Override
 	public void start(Stage base) throws Exception {
 		base.setTitle(appName);
@@ -24,8 +27,25 @@ public class SubRedditWallpaperMain extends Application{
 		//TODO: If download_path in srwe_config.txt is null, then show Select Directory dialog to select a default location.
 		
 		title.setStyle(titleCSSStyle);
-		TextField subredditLinkInput = new TextField(defaultSubLink);
+		 subredditLinkInput= new TextField(defaultSubLink);
 		Button runButton = new Button("Run");
+		
+		runButton.setOnAction( new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				String link = subredditLinkInput.getText();
+				ImageScraper scraper = new ImageScraper(link);
+				int res =scraper.scrapeImages();
+				if(res==0) {
+					System.out.println("Success!");
+				}
+				else {
+					System.out.println("FAIL");
+				}
+			}
+			
+		});
 		//TODO: Run the image scraper when button is clicked.
 		root.setTop(title);
 		root.setCenter(subredditLinkInput);
